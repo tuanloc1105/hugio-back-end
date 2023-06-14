11b25555c1a417bd029651143d6184733e650055
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-06-14 20:17:01 +0700

    Update code 2023-06-14 20:17:01

---
+++ push.sh
@@ -0,0 +1,8 @@
+c=$(date -d "$b 0 min" "+%Y-%m-%d %H:%M:%S")
+
+git add -A
+git commit -m "Update code ${c}"
+git push origin master
+echo "=============================== DONE ==============================="
+
+sleep 5m
