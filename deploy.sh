b9ff6e7f03a137d411ae3edfec93f26d1f4df794
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-06-27 20:59:08 +0700

    Update code 2023-06-27 20:59:07

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
+
+cd hugio-auth-service
+docker build . -t tuanloc/project:auth-service
+helm upgrade -i -n default auth-service ./helm_chart
+cd..
+
+cd hugio-product-service
+docker build . -t tuanloc/project:product-service
+helm upgrade -i -n default product-service ./helm_chart
+cd..
+
+cd hugio-user-service
+docker build . -t tuanloc/project:user-service
+helm upgrade -i -n default user-service ./helm_chart
+cd..
