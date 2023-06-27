cd3f58dcdcec41c5961454933b76851b236f49a6
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-06-27 21:01:53 +0700

    Update code 2023-06-27 21:01:53

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,38 @@
 
 mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
 
+echo "================================== tuanloc/project:auth-service =================================="
 cd hugio-auth-service
+echo "BUILD IMAGE"
 docker build . -t tuanloc/project:auth-service
+echo "PUSH IMAGE"
+docker push tuanloc/project:auth-service
+echo "UPGRADE HELM"
 helm upgrade -i -n default auth-service ./helm_chart
+echo "REMOVE IMAGE"
+docker rmi tuanloc/project:auth-service
 cd..
 
+echo "================================== tuanloc/project:product-service =================================="
 cd hugio-product-service
+echo "BUILD IMAGE"
 docker build . -t tuanloc/project:product-service
+echo "PUSH IMAGE"
+docker push tuanloc/project:product-service
+echo "UPGRADE HELM"
 helm upgrade -i -n default product-service ./helm_chart
+echo "REMOVE IMAGE"
+docker rmi tuanloc/project:product-service
 cd..
 
+echo "================================== tuanloc/project:user-service =================================="
 cd hugio-user-service
+echo "BUILD IMAGE"
 docker build . -t tuanloc/project:user-service
+echo "PUSH IMAGE"
+docker push tuanloc/project:user-service
+echo "UPGRADE HELM"
 helm upgrade -i -n default user-service ./helm_chart
+echo "REMOVE IMAGE"
+docker rmi tuanloc/project:user-service
 cd..
