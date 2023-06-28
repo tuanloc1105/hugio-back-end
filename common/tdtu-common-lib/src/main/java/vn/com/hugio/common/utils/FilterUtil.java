62505aab20d05b129b751e47a942f0f681d7cf1b
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-06-28 21:32:19 +0700

    Update code 2023-06-28 21:32:19

--- common/tdtu-common-lib/src/main/java/vn/com/hugio/common/utils/FilterUtil.java
+++ common/tdtu-common-lib/src/main/java/vn/com/hugio/common/utils/FilterUtil.java
@@ -59,9 +59,9 @@ public class FilterUtil {
 
     public void addCorsHeader(ServletResponse servletResponse) {
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Max-Age", "3600");
-        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, credential, X-XSRF-TOKEN");
+        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, credential, X-XSRF-TOKEN, Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
     }
 
     public void returnErrorResponse(ServletResponse servletResponse, Throwable e) {
