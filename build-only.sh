e5c3c37242668cfd0a2e3ea47bd850e61984ffe8
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-07-05 19:32:06 +0700

    Update code 2023-07-05 19:32:06

--- build-only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
+tag=$(date -d "$b 0 min" "+%Y_%m_%d_%H_%M_%S")
+auth="tuanloc/auth-service"
+product="tuanloc/product-service"
+user="tuanloc/user-service"
+
 mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
 
-echo "================================== tuanloc/hugio:auth-service =================================="
+echo "================================== $auth:$tag =================================="
 cd hugio-auth-service
 echo "BUILD IMAGE"
-docker build . -t tuanloc/hugio:auth-service
+docker build . -t $auth:$tag
 echo "PUSH IMAGE"
 docker push tuanloc/hugio:auth-service
 echo "UPGRADE HELM"
-helm upgrade -i -n hugio auth-service ./helm_chart
+helm upgrade -i --set image.name=$auth,image.tag=$tag -n hugio auth-service ./helm_chart
 echo "REMOVE IMAGE"
 docker rmi tuanloc/hugio:auth-service
 cd ..
 
-echo "================================== tuanloc/hugio:product-service =================================="
+echo "================================== $product:$tag =================================="
 cd hugio-product-service
 echo "BUILD IMAGE"
-docker build . -t tuanloc/hugio:product-service
+docker build . -t $product:$tag
 echo "PUSH IMAGE"
 docker push tuanloc/hugio:product-service
 echo "UPGRADE HELM"
-helm upgrade -i -n hugio product-service ./helm_chart
+helm upgrade -i --set image.name=$product,image.tag=$tag -n hugio product-service ./helm_chart
 echo "REMOVE IMAGE"
 docker rmi tuanloc/hugio:product-service
 cd ..
 
-echo "================================== tuanloc/hugio:user-service =================================="
+echo "================================== $user:$tag =================================="
 cd hugio-user-service
 echo "BUILD IMAGE"
-docker build . -t tuanloc/hugio:user-service
+docker build . -t $user:$tag
 echo "PUSH IMAGE"
 docker push tuanloc/hugio:user-service
 echo "UPGRADE HELM"
-helm upgrade -i -n hugio user-service ./helm_chart
+helm upgrade -i --set image.name=$user,image.tag=$tag -n hugio user-service ./helm_chart
 echo "REMOVE IMAGE"
 docker rmi tuanloc/hugio:user-service
 cd ..
