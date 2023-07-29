164c72093a56b97ea9e84cafa28ac59628078731
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-07-29 13:26:01 +0700

    Update code 2023-07-29 13:26:01

--- hugio-order-service/src/main/java/vn/com/hugio/order/entity/Order.java
+++ hugio-order-service/src/main/java/vn/com/hugio/order/entity/Order.java
@@ -1,6 +1,10 @@
 package vn.com.hugio.order.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,6 +12,7 @@ import lombok.NoArgsConstructor;
 import vn.com.hugio.common.entity.BaseEntity;
 
 import java.io.Serial;
+import java.util.List;
 
 @NoArgsConstructor
 @Data
@@ -18,5 +23,10 @@ public class Order extends BaseEntity {
     @Serial
     private static final long serialVersionUID = 690221254719037208L;
 
+    @Column(name = "TOTAL_PRICE")
+    private Double totalPrice;
+
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+    private List<OrderDetail> orderDetails;
 
 }
---
+++ hugio-order-service/src/main/java/vn/com/hugio/order/entity/OrderDetail.java
@@ -0,0 +1,35 @@
+package vn.com.hugio.order.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import vn.com.hugio.common.entity.BaseEntity;
+
+import java.io.Serial;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "USERS")
+@EqualsAndHashCode(callSuper = false)
+public class OrderDetail extends BaseEntity {
+
+
+    @Serial
+    private static final long serialVersionUID = -4542143539459709114L;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID", referencedColumnName = "ID", nullable = false)
+    private Order order;
+
+    @Column(name = "PRODUCT_UID")
+    private String productUid;
+
+    @Column(name = "QUANTITY")
+    private Long quantity;
+}
