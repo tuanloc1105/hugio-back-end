87418c57039e6fbf8184413e60dfd014173c6925
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-07-29 14:33:31 +0700

    Update code 2023-07-29 14:33:30

---
+++ hugio-product-service/src/main/java/vn/com/hugio/product/service/grpc/server/GrpcServer.java
@@ -0,0 +1,61 @@
+package vn.com.hugio.product.service.grpc.server;
+
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
+import vn.com.hugio.common.exceptions.ErrorCodeEnum;
+import vn.com.hugio.common.exceptions.InternalServiceException;
+import vn.com.hugio.common.log.LOG;
+import vn.com.hugio.grpc.inventory.ResponseTypeVoid;
+import vn.com.hugio.grpc.product.ProductInfoOutput;
+import vn.com.hugio.grpc.product.ProductServiceGrpc;
+import vn.com.hugio.grpc.product.RequestTypeProductInfoInput;
+import vn.com.hugio.grpc.product.ResponseTypeProductInfoOutput;
+import vn.com.hugio.product.dto.ProductDto;
+import vn.com.hugio.product.service.ProductService;
+import vn.com.hugio.proto.utils.GrpcUtil;
+
+@GrpcService
+@RequiredArgsConstructor
+public class GrpcServer extends ProductServiceGrpc.ProductServiceImplBase {
+
+    private final ProductService productService;
+
+    @Override
+    public void getProductDetail(RequestTypeProductInfoInput request, StreamObserver<ResponseTypeProductInfoOutput> responseObserver) {
+        ResponseTypeProductInfoOutput.Builder responseBuilder = ResponseTypeProductInfoOutput.newBuilder();
+        try {
+            GrpcUtil.getTraceId(request.getTrace());
+            LOG.info("RETRIEVE A GRPC MESSAGE");
+        } catch (RuntimeException e) {
+            responseBuilder.setCode(ErrorCodeEnum.VALIDATE_FAILURE.getCode().toString());
+            responseBuilder.setMessage(e.getMessage());
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+            return;
+        }
+        try {
+            ProductDto dto = this.productService.getProductDetail(request.getRequest().getUid());
+            ProductInfoOutput output = ProductInfoOutput.newBuilder()
+                    .setId(dto.getId())
+                    .setProductUid(dto.getProductUid())
+                    .setProductName(dto.getProductName())
+                    .setRawProductName(dto.getRawProductName())
+                    .setPrice(dto.getPrice())
+                    .setDiscount(dto.getDiscount())
+                    .setProductDescription(dto.getProductDescription())
+                    .build();
+            responseBuilder.setResponse(output);
+        } catch (InternalServiceException e) {
+            responseBuilder.setCode(e.getCode());
+            responseBuilder.setMessage(e.getMessage());
+        } catch (Exception e) {
+            responseBuilder.setCode(ErrorCodeEnum.FAILURE.getCode().toString());
+            responseBuilder.setMessage(e.getMessage());
+        }
+        responseBuilder.setTrace(GrpcUtil.createTraceTypeGrpc());
+        LOG.info("RETURN GRPC RESULT");
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+}
