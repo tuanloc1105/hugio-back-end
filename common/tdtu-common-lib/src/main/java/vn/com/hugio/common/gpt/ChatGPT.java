f64a470dfa5b1960b0740556b3b3822195a5dfab
Loc Vo Tuan <tuanloc1105@gmail.com>
2023-08-02 20:50:02 +0700

    Add ChatGPT On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   common/tdtu-common-lib/src/main/java/vn/com/hugio/common/gpt/ChatGPT.java

---
+++ common/tdtu-common-lib/src/main/java/vn/com/hugio/common/gpt/ChatGPT.java
@@ -0,0 +1,37 @@
+package vn.com.hugio.common.gpt;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+public class ChatGPT {
+    public static void chatGPT(String text) throws Exception {
+        String url = "https://api.openai.com/v1/completions";
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", "Bearer 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");
+
+        JSONObject data = new JSONObject();
+        data.put("model", "text-davinci-003");
+        data.put("prompt", text);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+
+        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
+                .reduce((a, b) -> a + b).get();
+
+        System.out.println(new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        chatGPT("How to create a simple chat bot");
+    }
+}
