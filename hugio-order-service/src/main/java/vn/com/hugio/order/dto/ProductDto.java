27aef8a3ab3f6da222a11c13f8c9aabb5a3e5227
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-08-15 20:01:40 +0700

    Update ProductDto
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
            modified:   hugio-order-service/src/main/java/vn/com/hugio/order/dto/ProductDto.java

--- hugio-order-service/src/main/java/vn/com/hugio/order/dto/ProductDto.java
+++ hugio-order-service/src/main/java/vn/com/hugio/order/dto/ProductDto.java
@@ -8,11 +8,12 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import vn.com.hugio.common.dto.BaseEntityDto;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Builder
 public class ProductDto extends BaseEntityDto {
 
     @JsonProperty("product_uid")
@@ -33,4 +34,25 @@ public class ProductDto extends BaseEntityDto {
     @JsonProperty("product_description")
     private String productDescription;
 
+    @Builder
+    public ProductDto(Long id,
+                      boolean active,
+                      LocalDateTime createdAt,
+                      LocalDateTime updatedAt,
+                      String createdBy,
+                      String updatedBy,
+                      String productUid,
+                      String productName,
+                      String rawProductName,
+                      Double price,
+                      Double discount,
+                      String productDescription) {
+        super(id, active, createdAt, updatedAt, createdBy, updatedBy);
+        this.productUid = productUid;
+        this.productName = productName;
+        this.rawProductName = rawProductName;
+        this.price = price;
+        this.discount = discount;
+        this.productDescription = productDescription;
+    }
 }
