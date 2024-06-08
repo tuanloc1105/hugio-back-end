eb64aa249ed9103759910f4802e414293e80b780
tuanloc1105 <tuanloc1105@gmail.com>
2024-06-08 11:59:01 +0700

    remove file

--- common/build.sh
+++ common/build.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+CURRENT_DIRECTORY=$(pwd)
+
+mvn dependency:resolve
+
+protoc -I=./tdtu-proto-lib/src/main/proto --java_out=./tdtu-proto-lib/src/main/java ./tdtu-proto-lib/src/main/proto/*.proto
+
 mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
 
-sleep 5m
+cd tdtu-proto-lib/
+
+git reset .
+
+git restore .
+
+cd ${CURRENT_DIRECTORY}
