93c6b68d732bf26344fec1f91e1566e48d161b0b
tuanloc1105 <tuanloc1105@gmail.com>
2024-06-08 18:52:27 +0700

    add protoc compiler for source

--- common/build.sh
+++ common/build.sh
@@ -4,7 +4,7 @@
 
 echo -e '\n\n >> Generate Java code \n\n'
 
-protoc -I=./tdtu-proto-lib/src/main/proto --java_out=./tdtu-proto-lib/src/main/java --grpc-java_out=./tdtu-proto-lib/src/main/java ./tdtu-proto-lib/src/main/proto/*.proto
+./protoc/bin/protoc -I=./tdtu-proto-lib/src/main/proto --java_out=./tdtu-proto-lib/src/main/java --grpc-java_out=./tdtu-proto-lib/src/main/java ./tdtu-proto-lib/src/main/proto/*.proto
 
 echo -e '\n\n >> Convert javax package to jakarta \n\n'
 
