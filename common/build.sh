7b9d123633760b47bbb57c84adee74bf9a6a13af
tuanloc1105 <tuanloc1105@gmail.com>
2024-06-08 14:17:19 +0700

    update proto build script

--- common/build.sh
+++ common/build.sh
@@ -4,14 +4,22 @@ clear
 
 CURRENT_DIRECTORY=$(pwd)
 
-mvn dependency:resolve
+# mvn dependency:resolve
+
+echo -e '\n\n >> Generate Java code \n\n'
 
 protoc -I=./tdtu-proto-lib/src/main/proto --java_out=./tdtu-proto-lib/src/main/java --grpc-java_out=./tdtu-proto-lib/src/main/java ./tdtu-proto-lib/src/main/proto/*.proto
+
+echo -e '\n\n >> Convert javax package to jakarta \n\n'
+
+python ./convert-javax-to-jakarta.py
+
+echo -e '\n\n >> Build maven \n\n'
 
 mvn clean install -DskipTests=true -Dfile.encoding=UTF8 -f pom.xml
 
 cd tdtu-proto-lib/
 
-rm -rf ./tdtu-proto-lib/src/main/java/vn/com/hugio/grpc/
+# rm -rf ./src/main/java/vn/com/hugio/grpc/
 
 cd ${CURRENT_DIRECTORY}
